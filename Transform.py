2a3d8d37251f615e04ccd0be7e9cd68ab9a6ac95
nantsing <nanqing_zhao@sjtu.edu.cn>
2023-04-10 09:58:11 +0800

    version2

--- Transform.py
+++ Transform.py
@@ -8,7 +8,9 @@ K = np.array([
     [0, 0, 1],
 ])
 
-def q2R(x, y, z, w):
+# 
+def q2R(q):
+    x, y, z, w = q
     r1 = 1 - 2 * y**2 - 2 * z**2
     r2 = 2 * x * y - 2 * w * z 
     r3 = 2 * x * z + 2 * w * y
@@ -23,8 +25,32 @@ def q2R(x, y, z, w):
         [r4, r5, r6],
         [r7, r8, r9],
     ])
-
-
-with open('camera.txt', 'r') as file:
-    print(file.read().split('\n'))
     
+    return R
+    
+def f2qt(f, q, t):
+    for i, item in enumerate(f):
+        if i < 3: t.append(float(item))
+        else: q.append(float(item))
+    
+def Transform(K1, R1, t1, K2, R2, t2):
+    c1 = -R1.T @ K1.inv() @ t1
+    c2 = -R2.T @ K1.inv() @ t1
+    
+
+
+if __name__ == '__main__':
+    
+    q1 = []
+    t1 = []
+    q2 = []
+    t2 = []
+    with open('camera.txt', 'r') as file:
+        files = file.read().split('\n')
+        f1 = files[0].split(' ')
+        f2 = files[1].split(' ')
+    f2qt(f1, q1, t1)
+    f2qt(f2, q2, t2)
+    R1 = q2R(q1)
+    R2 = q2R(q2)
+    T1, T2 = Transform(K, R1, t1, K, R2, t2)    
